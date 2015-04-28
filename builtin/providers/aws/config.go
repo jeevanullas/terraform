a8e4178f8b3e8e813cdb32b85274530a93c8bf79
Deependra Singh Shekhawat <jeevanullas@gmail.com>
2015-04-28 09:26:00 +0000

    Add the Eucalyptus region

--- builtin/providers/aws/config.go
+++ builtin/providers/aws/config.go
@@ -108,7 +108,7 @@ func (c *Config) Client() (interface{}, error) {
 // IsValidRegion returns true if the configured region is a valid AWS
 // region and false if it's not
 func (c *Config) ValidateRegion() error {
-	var regions = [11]string{"us-east-1", "us-west-2", "us-west-1", "eu-west-1",
+	var regions = [12]string{"us-east-1", "us-west-2", "us-west-1", "eucalyptus", "eu-west-1",
 		"eu-central-1", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1",
 		"sa-east-1", "cn-north-1", "us-gov-west-1"}
 
